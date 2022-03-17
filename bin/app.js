bca94f07c155b1f05f0ca1180e93953f30c1a6ba
UFAX24 <87823194+UFAX24@users.noreply.github.com>
2022-03-18 00:16:54 +0700

    Update app.js

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,5 @@
-#!/usr/bin/env node 
-
-const NodeMediaServer = require('..');
+const NodeMediaServer = require('node-media-server');
+//const fs = require('fs');
 
 const config = {
   rtmp: {
@@ -9,77 +8,33 @@ const config = {
     gop_cache: true,
     ping: 30,
     ping_timeout: 60,
-    ssl: {
-       port: 443,
-       key: __dirname+'/privatekey.pem',
-       cert: __dirname+'/certificate.pem',
-     }
   },
   http: {
     port: 8000,
-    mediaroot: __dirname+'/media',
-    webroot: __dirname+'/www',
+    mediaroot: './media',
     allow_origin: '*',
     api: true
   },
-  https: {
-    port: 8433,
-    key: __dirname+'/privatekey.pem',
-    cert: __dirname+'/certificate.pem',
-  },
-  auth: {
-    api: true,
-    api_user: 'admin',
-    api_pass: 'admin',
-    play: false,
-    publish: false,
-    secret: 'nodemedia2017privatekey'
+  trans: {
+    //ffmpeg: '/usr/bin/ffmpeg',
+    ffmpeg: '/usr/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        vc: "copy",
+        vcParam: [],
+        ac: "aac",
+        acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
+        rtmp:true,
+        rtmpApp:'live2',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
   }
 };
 
-
-let nms = new NodeMediaServer(config);
+var nms = new NodeMediaServer(config)
 nms.run();
-
-nms.on('preConnect', (id, args) => {
-  console.log('[NodeEvent on preConnect]', `id=${id} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
-
-nms.on('postConnect', (id, args) => {
-  console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-nms.on('doneConnect', (id, args) => {
-  console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-nms.on('prePublish', (id, StreamPath, args) => {
-  console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
-
-nms.on('postPublish', (id, StreamPath, args) => {
-  console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('donePublish', (id, StreamPath, args) => {
-  console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('prePlay', (id, StreamPath, args) => {
-  console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
-
-nms.on('postPlay', (id, StreamPath, args) => {
-  console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('donePlay', (id, StreamPath, args) => {
-  console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
